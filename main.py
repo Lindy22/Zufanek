5a1e0dce8a1b6d2a3a0354032948c7bd6876cb90
Tomas Lindauer <tomaslindauer@gmail.com>
2022-02-06 10:10:15 +0100

    nová verze
    - přidání metody pro spouštění na raspberry

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import datetime
 import smtplib
 import random
+import urllib3
+import lxml.etree
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,6 +19,7 @@ password = "raspibot99"
 hour = [6, 7, 8, 11, 13, 15, 17, 19, 21, 23]
 minute = [55, 56, 57]
 
+httpcon = urllib3.PoolManager()
 url = "https://www.lepsinalada.cz/"
 pripona = "/limitovane-edice/"
 
@@ -29,6 +32,22 @@ Mazej to koupit na: {url}
 
 TEMPLATE_IAMALIVE = """Robot pořád maká! Bohužel zatím se OMFG 2022 v nabídce ještě neobjevil.
 """
+
+
+def get_url(httpcon, url):
+    headers = {}
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    headers["Accept-Encoding"] = "gzip, deflate"
+    headers["Accept-Language"] = "cs,en-us;q=0.7,en;q=0.3"
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+    # headers["Cookie"]="op_cookie-test=ok;op_oddsportal=cv704mv1ek2l60ijve9iq9hn62;op_user_cookie=32830896;D_UID=739B93C4-5FB3-34AB-A4B5-0C2C36C3347E"
+    res = httpcon.urlopen("GET", url, headers=headers)
+
+    if res.status != 200:
+        print(res.status)
+        raise Exception("Unable to get_url " + url)
+    # print res.data
+    return res.data
 
 def execute_script():
     time.sleep(random.uniform(60, 120))
@@ -54,7 +73,7 @@ def send_email(username,password, fromaddr,toaddr,url):
     server.sendmail(fromaddr, toaddr, msg)
     server.quit()
 
-def get_token(url,pripona):
+def get_page_browser(url,pripona):
     browser = webdriver.Firefox()
     # time.sleep(10)
     browser.get(url+pripona)
@@ -82,9 +101,38 @@ def get_token(url,pripona):
             print("Je v nabídce!")
     browser.close()
 
+def get_page_raspi(httpcon, url,pripona):
+    print(url+pripona)
+    htmlparser = lxml.etree.HTMLParser()
+    page = get_url(httpcon, url+pripona)
+    p = lxml.etree.fromstring(page, htmlparser)
+    #print(''.join(p.itertext()).encode('utf-8'))
+    element = p.xpath(u'//div[@id="products"]/div[@class="product"]/div[@class="p"]/a/@href')
+    #print(element)
+    omfg_url = []
+    for elem_href in element:
+        if "omfg" in elem_href:
+            omfg_url.append(elem_href)
+        else:
+            pass
+    for urls in omfg_url:
+        #print(urls)
+        page_omfg = get_url(httpcon, url + urls)
+        p_omfg = lxml.etree.fromstring(page_omfg, htmlparser)
+        availability_xpath = p_omfg.xpath(u'//div[@class="availability-value"]/span[@class="availability-label"]/text()')
+        availability = availability_xpath[0].strip()
+        #print(availability)
+        if availability == "Vyprodáno":
+            print(availability)
+        else:
+            #send_email(username, password, fromaddr, lindy_mail, urls)
+            #send_email(username, password, fromaddr, gaba_mail, urls)
+            print("Je v nabídce!")
+
 while True:
     tstart = time.time()
-    get_token(url, pripona)
+    get_page_browser(url, pripona)
+    #get_page_raspi(httpcon, url, pripona)
     current_time = datetime.datetime.now()
     print(current_time)
     print("total time", time.time()-tstart)
